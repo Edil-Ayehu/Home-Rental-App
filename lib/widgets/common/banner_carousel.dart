828069fed591a3d9a70039796348922c8d15b8b3
Edilayehu Tadesse <edilayehu534027@gmail.com>
2025-02-10 00:27:16 -0800

    banner image caching

--- lib/widgets/common/banner_carousel.dart
+++ lib/widgets/common/banner_carousel.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:shimmer/shimmer.dart';
 import '../../models/banner_add.dart';
 import '../../core/constants/color_constants.dart';
 
@@ -13,6 +15,19 @@ class BannerCarousel extends StatefulWidget {
 
 class _BannerCarouselState extends State<BannerCarousel> {
   int _currentIndex = 0;
+
+  Widget _buildShimmerEffect() {
+    return Shimmer.fromColors(
+      baseColor: Colors.grey[300]!,
+      highlightColor: Colors.grey[100]!,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16.r),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,9 +56,21 @@ class _BannerCarouselState extends State<BannerCarousel> {
                   margin: EdgeInsets.symmetric(horizontal: 5.w),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(16.r),
-                    image: DecorationImage(
-                      image: NetworkImage(banner.imageUrl),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(16.r),
+                    child: CachedNetworkImage(
+                      imageUrl: banner.imageUrl,
                       fit: BoxFit.cover,
+                      placeholder: (context, url) => _buildShimmerEffect(),
+                      errorWidget: (context, url, error) => Container(
+                        color: Colors.grey[300],
+                        child: Icon(
+                          Icons.error_outline,
+                          color: AppColors.error,
+                          size: 32.sp,
+                        ),
+                      ),
                     ),
                   ),
                 );
